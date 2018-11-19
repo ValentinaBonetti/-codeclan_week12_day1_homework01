932dc39d5057cdc429fda3dc6823555a8750ae60
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-11-19 17:40:45 +0000

    Create the Employee abstract class

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,34 @@
+package staff;
+
+public abstract class Employee {
+
+    protected String name;
+    protected String NI;
+    protected Double salary;
+
+    public Employee(String name, String NI, Double salary) {
+        this.name = name;
+        this.NI = NI;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNI() {
+        return NI;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(Double salaryRaise){
+        this.salary += salaryRaise;
+    }
+
+    public Double payBonus(){
+        return this.salary*0.01;
+    }
+}
